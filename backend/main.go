2f99b511251ab6484f2a3deb63826d59ff3eeb36
Santiago C. Saavedra <zrock_git@zohomail.com>
2025-03-20 10:49:59 -0400

    fix: bad hostname var assignation

--- backend/main.go
+++ backend/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"log"
 	"os"
 
 	"github.com/gin-gonic/gin"
@@ -8,11 +9,24 @@ import (
 
 func main() {
 	router := gin.Default()
+	router.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "ok",
+		})
+	})
+
+	hostname, err := os.Hostname()
+	if err != nil {
+		log.Fatalf("Couldn't get hostname: %v", err)
+	}
+	
+
+
 	router.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
-			"hostname": os.Hostname,
+			"hostname": hostname,
 			"message":  "pong",
 		})
 	})
-	router.Run("localhost:3000")
+	router.Run()
 }
